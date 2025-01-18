2296f59fcbe409ad82fea304d8edbc5534bbad27
sakshivilas <sakshivilas.t@techouts.com>
2025-01-18 14:54:36 +0530

    Abstraction added

---
+++ src/cj5/AbstractPrep.java
@@ -0,0 +1,22 @@
+package cj5;
+
+abstract class car{
+	public abstract void drive();
+	
+}
+class wagnor extends car{
+	
+	public void drive()
+	{
+		System.out.println("driving");
+	}
+	
+}
+
+public class AbstractPrep {
+	public static void main(String[] args)
+	{
+		wagnor w = new wagnor();
+		w.drive();
+	}
+}
