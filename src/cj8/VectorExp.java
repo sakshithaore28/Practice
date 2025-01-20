6993280aa2c14db43f5c1c49bbfa79be7b946a01
sakshivilas <sakshivilas.t@techouts.com>
2025-01-20 12:11:12 +0530

    Vector added

---
+++ src/cj8/VectorExp.java
@@ -0,0 +1,26 @@
+package cj8;
+
+import java.util.*;  
+
+public class VectorExp {
+	
+
+	  
+	       public static void main(String args[]) {  
+	          //Create a vector  
+	          Vector<String> vec = new Vector<String>();  
+	           
+	          vec.add("Tiger");  
+	          vec.add("Lion");  
+	          vec.add("Dog");  
+	          vec.add("Elephant");  
+	          
+	          vec.addElement("Rat");  
+	          vec.addElement("Cat");  
+	          vec.addElement("Deer");  
+	            
+	          System.out.println("Elements are: "+vec);  
+	       }  
+	}  
+
+
