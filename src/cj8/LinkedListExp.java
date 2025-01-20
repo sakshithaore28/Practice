57322f0331f352756ea2b4e86b0a24ba5bb4ad80
sakshivilas <sakshivilas.t@techouts.com>
2025-01-20 11:45:59 +0530

    linkedList added

--- src/cj8/LinkedListExp.java
+++ src/cj8/LinkedListExp.java
@@ -1,17 +1,17 @@
 package cj8;
-import java.util.*;
+import java.util.*;  
 class Book {  
-	int id;  
-	String name,author,publisher;  
-	int quantity;  
-	public Book(int id, String name, String author, String publisher, int quantity) {  
-	    this.id = id;  
-	    this.name = name;  
-	    this.author = author;  
-	    this.publisher = publisher;  
-	    this.quantity = quantity;  
-	}
-
+int id;  
+String name,author,publisher;  
+int quantity;  
+public Book(int id, String name, String author, String publisher, int quantity) {  
+    this.id = id;  
+    this.name = name;  
+    this.author = author;  
+    this.publisher = publisher;  
+    this.quantity = quantity;  
+}  
+} 
 public class LinkedListExp {
 	public static void main(String[] args) {  
 	    //Creating list of Books  
@@ -29,12 +29,5 @@ public class LinkedListExp {
 	    System.out.println(b.id+" "+b.name+" "+b.author+" "+b.publisher+" "+b.quantity);  
 	    }  
 	}  
-	}  
+
 }
-	  
-	  
-	
-
-
-
-
