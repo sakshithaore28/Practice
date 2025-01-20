2af9869bafc21b488eec68c2f42de2be53862e3d
sakshivilas <sakshivilas.t@techouts.com>
2025-01-20 12:24:20 +0530

    Hashmap added

---
+++ src/cj8/HashMapExp.java
@@ -0,0 +1,20 @@
+package cj8;
+import java.util.*; 
+public class HashMapExp {
+	 
+	
+	 public static void main(String args[]){  
+	   HashMap<Integer,String> map=new HashMap<Integer,String>();//Creating HashMap    
+	   map.put(1,"Mango");  //Put elements in Map  
+	   map.put(2,"Apple");    
+	   map.put(3,"Banana");   
+	   map.put(1,"Grapes"); //trying duplicate key  
+	       
+	   System.out.println("Iterating Hashmap...");  
+	   for(Map.Entry m : map.entrySet()){    
+	    System.out.println(m.getKey()+" "+m.getValue());    
+	   }  
+	}  
+	}  
+
+
