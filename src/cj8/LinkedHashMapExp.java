f2384d6fbb4537c7eb3b7feb0d5b300898aa6d5d
sakshivilas <sakshivilas.t@techouts.com>
2025-01-20 12:51:13 +0530

    LinkedHashMap added

---
+++ src/cj8/LinkedHashMapExp.java
@@ -0,0 +1,24 @@
+package cj8;
+import java.util.*; 
+public class LinkedHashMapExp {
+	 
+
+	 
+	 public static void main(String args[]){  
+	   LinkedHashMap<Integer, String> map = new LinkedHashMap<Integer, String>();           
+
+	      map.put(100,"Amit");    
+	     map.put(101,"Vijay");    
+	     map.put(102,"Rahul");    
+
+	       
+	       System.out.println("Keys: "+map.keySet());  
+	       
+
+	       System.out.println("Values: "+map.values());  
+	        
+	       System.out.println("Key-Value pairs: "+map.entrySet());  
+
+	 }  
+	}  
+
