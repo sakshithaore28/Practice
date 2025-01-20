dcd72bcc45e44d94db659b01902a25ce292475ef
sakshivilas <sakshivilas.t@techouts.com>
2025-01-20 11:36:05 +0530

    ArrayList added

---
+++ src/cj8/ArratListExp.java
@@ -0,0 +1,22 @@
+package cj8;
+import java.util.*;
+
+
+public class ArratListExp {
+	  
+	
+	 public static void main(String args[]){  
+	  ArrayList<String> list=new ArrayList<String>();//Creating arraylist  
+	  list.add("Mango");    
+	  list.add("Apple");    
+	  list.add("Banana");    
+	  list.add("Grapes");    
+	 
+	  for(String fruit:list)    
+	    System.out.println(fruit);    
+	  
+	 }  
+
+	}  
+
+
