eb53de2c21cab6b67d195e68bb5c4fee9d0fc202
sakshivilas <sakshivilas.t@techouts.com>
2025-01-20 17:00:10 +0530

    LinkedHashSet added

---
+++ src/cj7/LinkedHashSetExp.java
@@ -0,0 +1,22 @@
+package cj7;
+import java.util.*;  
+
+public class LinkedHashSetExp {
+	
+	
+	 public static void main(String args[]){  
+	 //Creating HashSet and adding elements  
+	        LinkedHashSet<String> set=new LinkedHashSet();  
+	               set.add("One");    
+	               set.add("Two");    
+	               set.add("Three");   
+	               set.add("Four");  
+	               set.add("Five");  
+	               Iterator<String> i=set.iterator();  
+	               while(i.hasNext())  
+	               {  
+	               System.out.println(i.next());  
+	               }  
+	 }  
+	} 
+
